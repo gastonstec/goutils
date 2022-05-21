9b351d80f66f34685aceaa1c87fb4a3e7114b9ef
Gaston Sanchez <gastons@exatec.tec.mx>
2022-05-21 11:04:10 -0500

    feat(goutils): initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gastonstec/goutils
+
+go 1.18
---
+++ goutils.go
@@ -0,0 +1,72 @@
+// Package utils provides some commonly used functions.
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+)
+
+
+// Function IsJSON checks if the supplied string is in a
+// valid JSON format. Returns true if the string is valid,
+// false if not
+func IsJSON(str string) bool {
+	var js json.RawMessage
+
+	err := json.Unmarshal([]byte(str), &js)
+	
+	return err == nil
+}
+
+
+// Function GetFunctionName returns the name of the current
+// running function
+func GetFunctionName() string {
+	pc := make([]uintptr, 1)
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[0])
+	return f.Name()
+}
+
+
+// Function GetOSEnv returns the operating system
+// environment variables in a JSON format string.
+func GetOsEnv() string {
+
+	var env []string = os.Environ()
+
+	envStr := "{\"OSEnvironment\": {"
+
+	for index, value := range env {
+		name := strings.Split(value, "=") // split by = sign
+		name[1] = strings.ReplaceAll(name[1], "\\", "/")
+		name[1] = strings.ReplaceAll(name[1], ":", ".")
+		name[1] = strings.ReplaceAll(name[1], "\"", "")
+		envStr = envStr + (fmt.Sprintf("\"%d\": \"%s=%s\",", index, name[0], name[1]))
+	}
+
+	envStr = strings.TrimRight(envStr, ",")
+	envStr = envStr + "}}"
+
+	return envStr
+}
+
+// Function GetGolangEnv returns the golang
+// environment variables in a JSON format string.
+func GetGolangEnv() string {
+
+	// set environment string
+	envStr := fmt.Sprintf("{\"GolangEnvironment\": {\"Version\": \"%s\", \"GOMAXPROCS\": %d, \"NumCPU\": %d, \"GOOS\": \"%s\", \"GOARCH\": \"%s\", \"GOROOT\": \"%s\", \"Compiler\": \"%s\"}}",
+		runtime.Version(),
+		runtime.GOMAXPROCS(0),
+		runtime.NumCPU(),
+		strings.ReplaceAll(strings.ReplaceAll(runtime.GOOS, "\\", "/"), ":", "."),
+		strings.ReplaceAll(strings.ReplaceAll(runtime.GOARCH, "\\", "/"), ":", "."),
+		strings.ReplaceAll(strings.ReplaceAll(runtime.GOROOT(), "\\", "/"), ":", "."),
+		runtime.Compiler)
+
+	return envStr
+}
